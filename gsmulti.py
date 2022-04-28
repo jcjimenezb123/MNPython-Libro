f0c4e6e403d587882266cab1e01b28570c89f4a9
jcjimenezb123 <55670828+jcjimenezb123@users.noreply.github.com>
2022-04-28 14:52:51 -0500

    Add files via upload

--- gsmulti.py
+++ gsmulti.py
@@ -18,10 +18,10 @@ def gsm(g,x,imax=200,tol=1e-8):
         raise ValueError ('El sistema no converge')
 
 def g(x):
-    xk1=x.copy()
-    xk1[0]=np.sqrt(x[1]**2/5)
-    xk1[1]=0.25*(np.sin(x[0])+np.cos(x[1]))
-    return xk1
+    x1,x2=x
+    x1=np.sqrt(x2**2/5) 
+    x2=0.25*(np.sin(x1)+np.cos(x2))
+    return np.array([x1,x2])
 
 def main():
     x=np.array([0.5,0.5])
